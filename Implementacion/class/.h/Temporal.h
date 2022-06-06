9fb9f98a624a34172acb895e49c057b606ecf886
CintiaLeal <cintialeal31m@gmail.com>
2022-06-06 11:33:12 -0300

    diagramas guzman

---
+++ Implementacion/class/.h/Temporal.h
@@ -0,0 +1,26 @@
+#ifndef Temporal_
+#define Temporal_
+
+#include <iostream>
+#include <Suscripcion.h>
+
+//CAMBIAR LA RUTA DE LA INCLUSION 
+
+using namespace std;
+
+class Temporal : public Suscripcion{
+    private:
+        string periodo;
+    public:
+        //constructor
+        Temporal();
+        Temporal(string, int, string);
+        //get
+        string getPeriodo();
+        //set
+        void setPeriodo(string);
+        //methods
+        tipoSuscripcion getTipo();
+};
+
+#endif
---
+++ Implementacion/class/.h/Vitalicia.h
@@ -0,0 +1,23 @@
+#ifndef Vitalicia_
+#define Vitalicia_
+
+#include <iostream>
+#include <Suscripcion.h>
+
+//CAMBIAR LA RUTA DE LA INCLUSION 
+
+using namespace std;
+
+class Vitalicia : public Suscripcion{
+    private:
+    
+    public:
+        //constructor
+        Vitalicia();
+        Vitalicia(string, int);
+        //methods
+        bool buscarSuscripcion();
+        tipoSuscripcion getTipo();
+};
+
+#endif

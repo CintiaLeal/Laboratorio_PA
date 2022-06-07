dd009e5e64090bf7d758885d858f57090ee0e72c
GuzmanVera <guzvera.liceo@gmail.com>
2022-06-07 10:37:46 -0300

    actualizacion uml

---
+++ Implementacion/class/.cpp/Partida.cpp
@@ -0,0 +1,11 @@
+#include "class/.h/Partida.h"
+#include <iostream>
+
+using namespace std;
+
+//constructores
+Partida::Partida(){}
+
+Partida::Partida(int idPartida, dtPartida* partida, dtUsuario* jugadores, dtVideojuego*){
+    this->idPartida = 
+}

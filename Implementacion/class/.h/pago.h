83c604e73c3cfa407f5526daceee3194391b6414
CintiaLeal <cintialeal31m@gmail.com>
2022-06-09 10:51:34 -0300

    Arreglos Pago.h

--- Implementacion/class/.h/pago.h
+++ Implementacion/class/.h/pago.h
@@ -4,19 +4,28 @@
 #include <iostream>
 #include "../../dtType/.h/dtFecha.h"
 #include "../../dtType/.h/dtVideojuego.h"
-//#include "class/.h/Videojuego.h"
+#include "Suscripcion.h"
+#include "../../dtType/.h/dtPartidaIndividual.h"
+#include "../../dtType/.h/dtPartida.h"
+#include "../../dtType/.h/dtUsuario.h"
+#include "../../dtType/.h/dtMostrarPartida.h"
+#include "../../ICollection/interfaces/ICollectible.h"
+#include "../../ICollection/collections/OrderedDictionary.h"
+#include "../../ICollection/String.h"
+#include "../../ICollection/Integer.h"
 using namespace std;
 
-class Pago{
+class Pago:public ICollectible{
     private:
         dtFecha fechaV;
         dtFecha fechaContrato;
         string metodoPago;
         bool activa;
+        Suscripcion * suscripcion;
     public:
         //constructores
         Pago();
-        Pago(dtFecha, dtFecha, string, bool);
+        Pago(dtFecha, dtFecha, string, bool, Suscripcion*);
         //gets
         dtFecha getFechaV();
         dtFecha getFechaC();
